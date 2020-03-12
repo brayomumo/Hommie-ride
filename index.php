0119ea536f33bfb57f27416200854624babfe4bf
brayomumo <brayomumo5@gmail.com>
2020-03-12 11:04:07 +0300

    '

--- index.php
+++ index.php
@@ -10,5 +10,18 @@
         include('navbar.php');
         ?>
         <h2> Welcome Sucker </h2>
+        <form action="" method="POST">
+            <input type="radio" name="radio" value="radio 1">Radio 1
+            <input type="radio" name="radio" value="radio 1">Radio 2
+            <input type="radio" name="radio" value="radio 1">Radio 3
+            <input type="submit" name="submit" value="Get selected item">
+        </form>
+        <?php
+            if(isset($_POST['submit'])){
+                if(isset($_POST['radio'])){
+                    echo "You have selected:  ".$_POST['radio'];
+                }
+            }
+        ?>
 </body>
 </html>
