a7509b9fb1af322f471050338779697ecd5b1006
brayomumo <brayomumo5@gmail.com>
2020-04-23 17:00:25 +0300

    get group details

--- trial/api/group_functions.php
+++ trial/api/group_functions.php
@@ -1,5 +1,6 @@
 <?php
 require("../auth.php");
+//joining a group
 function joinGroup($id)
 {
     require('../db.php');
@@ -31,7 +32,7 @@ function createPost($news)
     require('../db.php');
     $id = $_SESSION['id'];
     $q = "SELECT group_id FROM  user_group where user_id = '$id'";
-    $tri = mysqli_query($con,$q ) or die(mysqli_error($con));
+    $tri = mysqli_query($con, $q) or die(mysqli_error($con));
     $tr = mysqli_fetch_assoc($tri);
     if ($tri) {
         $g_id = $tr["group_id"];
@@ -47,37 +48,52 @@ function createPost($news)
 }
 // getting posts
 function showposts($g_id)
-    {
-        require("../db.php");
-        $query = "SELECT users.username , posts.* FROM posts LEFT OUTER JOIN users on users.user_id = posts.userID where group_id='$g_id'";
-        $posts = mysqli_query($con, $query) or die(mysqli_error($con));
-        $allposts = Array();
-        if ($posts) {
-            if (mysqli_num_rows($posts) > 0) {
-                while ($row = mysqli_fetch_array($posts)) {
-                $obj = array_fill_keys(array("username","post","whenPosted"),"");
+{
+    require("../db.php");
+    $query = "SELECT users.username , posts.* FROM posts LEFT OUTER JOIN users on users.user_id = posts.userID where group_id='$g_id'";
+    $posts = mysqli_query($con, $query) or die(mysqli_error($con));
+    $allposts = array();
+    if ($posts) {
+        if (mysqli_num_rows($posts) > 0) {
+            while ($row = mysqli_fetch_array($posts)) {
+                $obj = array_fill_keys(array("username", "post", "whenPosted"), "");
                 $obj["username"] = $row['username'];;
-                $obj["post"] =  $row['post'] ;
+                $obj["post"] =  $row['post'];
                 $obj["whenPosted"] =  $row['when_posted'];
                 array_push($allposts, $obj);
-                }
-                mysqli_free_result($posts);
-                
-            } else {
-                echo 'No group news currently';
             }
-            return json_encode($allposts);
+            mysqli_free_result($posts);
+        } else {
+            echo 'No group news currently';
         }
+        return json_encode($allposts);
     }
-    function getGroupDetails($g_id){
-        
+}
+//getting group details
+function getGroupDetails($g_id)
+{
+    require("../db.php");
+    $q = "SELECT username, place_of_work,cartype, car_plate_number from users left outer join user_group ON user_group.user_id = users.user_id WHERE user_group.group_id = '$g_id'";
+    $query = mysqli_query($con, $q) or die(mysqli_error($con));
+    $details = Array();
+        if (mysqli_num_rows($query) > 0) {
+            while ($row = mysqli_fetch_array($query)) {
+                $obj = array_fill_keys(array("username", "placeofwork", "cartype","carplatenumber"), "");
+                $obj["username"] = $row['username'];;
+                $obj["placeofwork"] =  $row['place_of_work'];
+                $obj["cartype"] =  $row['cartype'];
+                $obj["carplatenumber"] =  $row['car_plate_number'];
+                array_push($details, $obj);
+            }
+            mysqli_free_result($query);
+            return json_encode($details);
     }
+}
 // choosing functions
 if (isset($_POST['g_id'])) {
     echo joinGroup($_POST['g_id']);
 } elseif (isset($_POST["post"])) {
     echo (createPost($_POST["post"]));
-}
-elseif(isset($_POST['getID'])){
+} elseif (isset($_POST['getID'])) {
     print_r(showposts($_POST['getID']));
 }
